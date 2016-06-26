e36b5c77fa2dc61c20d39693aecdd42e4612b69d
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-06-26 09:14:07 -0700

    Add standard meta to head

--- src/blocks/server/handler.clj
+++ src/blocks/server/handler.clj
@@ -19,6 +19,10 @@
        :body (html
                [:html
                 [:head
+                 [:meta {:charset "utf-8"}]
+                 [:meta {:http-equiv "x-ua-compatible" :content "ie=edge"}]
+                 [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
+                 [:meta {:name "generator" :content "blocks"}]
                  [:title (get-in page [:meta :title])]]
                 [:body
                  [:div {:id "app"}]
