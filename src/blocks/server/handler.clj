90ffd72aeb3a9aff30a1c7bee4b95e8de73b3461
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-07-10 13:24:58 -0700

    Factor out a few functions

--- src/blocks/server/handler.clj
+++ src/blocks/server/handler.clj
@@ -5,41 +5,54 @@
     [ring.middleware.not-modified :refer [wrap-not-modified]]
     [ring.middleware.content-type :refer [wrap-content-type]]))
 
-(defn render-page [domain url]
-  (let [blocks (read-string (slurp (clojure.java.io/resource "data/blocks.edn")))
-        pages (read-string (slurp (clojure.java.io/resource "data/pages.edn")))]
+(defn read-pages-edn []
+  (read-string (slurp (clojure.java.io/resource "data/pages.edn"))))
+
+(defn read-blocks-edn []
+  (read-string (slurp (clojure.java.io/resource "data/blocks.edn"))))
+
+(defn get-page-data [page]
+  (let [blocks (read-blocks-edn)]
+    {:page (-> page
+               (update-in [:blocks]
+                          (fn [bs]
+                            (vec (map (fn [block-id]
+                                        (blocks block-id)) bs)))))}))
+
+(defn path->page [domain url]
+  (let [pages (read-pages-edn)]
     (when-let [page (->> pages
                          (filter (fn [page]
                                    (and
                                      (= (page :url) url)
                                      (= (page :domain) domain))))
                          first)]
-      {:status 200
-       :headers {"Content-Type" "text/html"}
-       :body (html
-               [:html
-                [:head
-                 [:meta {:charset "utf-8"}]
-                 [:meta {:http-equiv "x-ua-compatible" :content "ie=edge"}]
-                 [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
-                 [:meta {:name "generator" :content "blocks"}]
-                 [:style {:type "text/css"}
-                  (slurp (clojure.java.io/resource "public/css/reset.css"))]
-                 [:title (get-in page [:meta :title])]]
-                [:body
-                 [:div {:id "app"}]
-                 [:script {:type "text/edn" :id "data"}
-                  (pr-str {:page (-> page
-                                     (update-in [:blocks]
-                                                (fn [bs]
-                                                  (vec (map (fn [block-id]
-                                                              (blocks block-id)) bs)))))})]
-                 [:script {:src "/js/blocks.js" :type "text/javascript"}]
-                 [:script {:type "text/javascript"}
-                  "blocks.client.core.run()"]]])})))
+      page)))
+
+(defn render-page [domain url]
+  (when-let [page (path->page domain url)]
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body (html
+             [:html
+              [:head
+               [:meta {:charset "utf-8"}]
+               [:meta {:http-equiv "x-ua-compatible" :content "ie=edge"}]
+               [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
+               [:meta {:name "generator" :content "blocks"}]
+               [:style {:type "text/css"}
+                (slurp (clojure.java.io/resource "public/css/reset.css"))]
+               [:title (get-in page [:meta :title])]]
+              [:body
+               [:div {:id "app"}]
+               [:script {:type "text/edn" :id "data"}
+                (pr-str (get-page-data page))]
+               [:script {:src "/js/blocks.js" :type "text/javascript"}]
+               [:script {:type "text/javascript"}
+                "blocks.client.core.run()"]]])}))
 
 (defn index-page []
- (let [pages (read-string (slurp (clojure.java.io/resource "data/pages.edn")))]
+ (let [pages (read-pages-edn)]
    {:status 200
     :headers {"Content-Type" "text/html"}
     :body (html
