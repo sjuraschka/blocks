da042850a256a1b9975f3994677df77bff08325b
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-09-18 19:32:08 -0400

    Improve raw template

--- src/blocks/client/templates/raw.cljs
+++ src/blocks/client/templates/raw.cljs
@@ -12,21 +12,40 @@
      {:font-weight "bold"
       :text-align "right"}]]])
 
-(defn table [data]
-  [:table
-   [:tbody
-    (for [[k v] data]
+(defn render [data]
+  (cond
+    (map? data)
+    [:table
+     [:tbody
       [:tr
-       [:td.key (name k)]
-       [:td
-        (if (map? v)
-          [table v]
-          (pr-str v))]])]])
+       [:td "{"]]
+      (for [[k v] data]
+        [:tr
+         [:td.key (name k)]
+         [:td
+          [render v]]])
+      [:tr
+       [:td "}"]]]]
+
+    (vector? data)
+    [:table
+     [:tbody
+      [:tr
+       [:td "["]]
+      (for [v data]
+        [:tr
+         [:td
+          [render v]]])
+      [:tr
+       [:td "]"]]]]
+
+    :else
+    [:div (pr-str data)]))
 
 (defn component [data]
   [:section.raw
    [:div.content
-    [table data]]])
+    [render data]]])
 
 (defmethod template "raw" [_]
   {:css styles
