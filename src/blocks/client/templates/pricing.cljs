800e1224b4fb66d353635200dbcd2e286d80c360
Sam Juraschka <samjuraschka@gmail.com>
2016-09-20 11:47:35 -0400

    Add styles to pricing block

--- src/blocks/client/templates/pricing.cljs
+++ src/blocks/client/templates/pricing.cljs
@@ -1,51 +1,74 @@
 (ns blocks.client.templates.pricing
-  (:require [blocks.client.template :refer [template]]))
+  (:require [blocks.client.template :refer [template]]
+            [blocks.client.templates.mixins :refer [button-mixin]]))
 
 (defn styles [data]
   [:section.pricing
    [:.options
-    {:display "flex"
+    {:display         "flex"
      :justify-content "space-between"
-     :padding "3em"
-     :align-items "stretch"}
+     :padding         "3em"
+     :align-items     "stretch"}
 
     [:.option
-     {:background "#fff"
-      :box-shadow [[0 "1px" "2px" 0 "#ccc"]]
-      :border-radius "3px"
-      :padding "1em"
-      :box-sizing "border-box"
-      :margin-right "2em"
-      :display "flex"
+     {:background     "#fff"
+      :box-shadow     [[0 "1px" "2px" 0 "#ccc"]]
+      :border-radius  "5px"
+      :border         "1px solid #eee"
+      :box-sizing     "border-box"
+      :margin-right   "2em"
+      :text-align     "center"
+      :display        "flex"
       :flex-direction "column"
-      :align-items "center"}
+      :align-items    "center"}
 
      [:&:last-child
       {:margin-right 0}]
 
      [:&.highlight
-      {:border "1px solid green"}]
+      {:border "3px solid #208456"}]
 
-     [:h1
-      {:color (get-in data [:title-color])
+     [:.about
+      {:height "15%"
+       :padding "1em 1em 5em 1em"}
+
+      [:h1
+      {:color       (get-in data [:title-color])
        :font-weight "bolder"
-       :font-size "1.25em"}]
+       :font-size   "1.25em"}]
+      [:h2
+       {:font-weight "bolder"}]
 
      [:.price
 
       [:.amount
+       {:color     "#444"
+        :font-size "2.5em;"}
 
        [:&::before
-        {:content "\"$\""
-         }
-        ]
-       ]]
+        {:content "\"$\""}]]
+
+      [:button
+       {:background "#208456"
+        :font-size "1em"
+        :font-weight "bolder"
+        :padding "0.25em"
+        :border-radius "0.25em"
+        :margin "1em"
+        :border "none"
+        :color "#fff"}]
+
+      [:.text
+       {:color "#ccc"}]]]
 
      [:.features
-      {:padding-top "1em"}
-      [:.feature ]]
 
-     ]]])
+      {:width       "100%"
+       :padding-top "1em"}
+      [:.feature
+       {:border-top "1px solid #eee"
+        :left       0
+        :right      0}]]]]])
 
 
 (defn component [data]
