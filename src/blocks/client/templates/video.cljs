4e316740e771fcfcb776aba162ec264a21557076
Sam Juraschka <samjuraschka@gmail.com>
2016-09-23 23:49:38 -0400

    Style video header to look distinct among other feature highlights

--- src/blocks/client/templates/video.cljs
+++ src/blocks/client/templates/video.cljs
@@ -4,53 +4,40 @@
 
 (defn styles [data]
   [:.video
+   {:padding "4em"
+    :background (get-in data [:styles :background])}
+
    [:.content
     {:display "flex"
-     :justify-content "space-around"
-     :padding "2em"}
-    [:.left
-     {:display         "flex"
-      :flex-direction  "column"
-      :justify-content "center"}
+     :flex-direction  "column"
+     :justify-content "center"
+     :align-items "center"
+     :text-align "center"}
 
      [:h1
       {:color (get-in data [:heading :color])
-       :font-size "2.2em"
-       :margin-bottom "0.5em"
+       :font-size "2.2rem"
+       :font-weight "bolder"
        :position "relative"
        :z-index 0}]
      [:h2
-      {:color (get-in data [:subtitle :color])}]]
-    [:.right
-     [:.player]]]
+      {:color (get-in data [:subtitle :color])
+       :font-size "1.25em"
+       :padding-bottom "2em"}]
 
-   (at-media {:max-width "750px"}
-             [:.content
-              {:display "flex"
-               :flex-direction "column"
-               :justify-content "center"
-               :align-items "center"}
-              [:.left
-               {:margin-left 0
-                :margin-right 0
-                :text-align "center"
-                :padding "1em"}
-               [:h1
-                {:font-size "2.2em"}]
-               [:h2
-                {:color (get-in data [:subtitle :color])}]]])])
+     [:.player
+      {:max-height "20em"}]
+    [:p {:padding "1em"
+         :color "#fff"}]]])
 
 (defn component [data]
   [:div.video
    [:div.content
-    [:div.left
      [:h1 (get-in data [:heading :title])]
-     [:h2 (get-in data [:subtitle :text])]]
-    [:div.right
-     [:iframe.player {:src  "https://www.youtube.com/embed/pa2bUsChFqM"
-                     :style {:height "252px"
-                             :width "448px"
-                             :frameborder 0}}]]]])
+     [:h2 (get-in data [:subtitle :text])]
+     [:img.player {:src "/images/video-player.png"}]
+     ;[:p "We hated chat."]
+    ]])
 
 (defmethod template "video" [_]
   {:css       styles
