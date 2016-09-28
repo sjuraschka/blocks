ae74d318747de94ae41820c6fae50362d4ef09a0
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-09-28 11:23:53 -0400

    Restyle header using flex-box

--- src/blocks/client/templates/header.cljs
+++ src/blocks/client/templates/header.cljs
@@ -8,63 +8,66 @@
    {:position "absolute"
     :top 0
     :left 0
-    :z-index 100
-    :width "100%"
-    :margin "3em 1em"}
+    :right 0
+    :z-index 100}
 
-   [:a.logo
-    {:display "inline-block"
-     :color (get-in data [:logo :color])}
+   [:.content
+    {:display "flex"
+     :width "100%"
+     :max-width "100%"
+     :padding "3em"
+     :box-sizing "border-box"
+     :justify-content "space-between"}
 
-    [:img
-     {:height "2rem"
-      :display "inline-block"
-      :vertical-align "middle"
-      :margin-right "0.5em"}]
+    [:a.logo
+     {:color (get-in data [:logo :color])}
 
-    [:h1
-     {:display "inline-block"
-      :font-size "1.25em"
-      :line-height "2rem"
-      :vertical-align "middle" }]
+     [:img
+      {:height "2rem"
+       :display "inline-block"
+       :vertical-align "middle"
+       :margin-right "0.5em"}]
 
-    [:.version
-     {:display "inline-block"
-      :opacity "0.5"
-      :font-size "0.75em"
-      :line-height "2rem"
-      :vertical-align "sub"
-      :margin-left "0.5em"
-      :text-transform "uppercase"
-      :letter-spacing "0.1em"}]]
-
-   [:nav
-    {:display "inline-block"
-     :position "absolute"
-     :right 0}
-
-    [:a
-     {:color (data :color)
-      :opacity 0.6
-      :text-decoration "none"
-      :margin-left "2em"
-      :transition "opacity 0.1s ease-in-out"
-      :line-height "2em"}
-
-     [:&:hover
-      {:opacity 1}]
-
-     ["&[data-icon]:before"
-      {:content "attr(data-icon)"
-       :font-family "FontAwesome"
-       :margin-right "0.25em"}]
-
-     [:&.button
+     [:h1
       {:display "inline-block"
-       :border [["1px" "solid" (data :color)]]
-       :border-radius "0.25em"
-       :height "2em"
-       :padding "0 0.5em" }]]]])
+       :font-size "1.25em"
+       :line-height "2rem"
+       :vertical-align "middle"}]
+
+     [:.version
+      {:display "inline-block"
+       :opacity "0.5"
+       :font-size "0.75em"
+       :line-height "2rem"
+       :vertical-align "sub"
+       :margin-left "0.5em"
+       :text-transform "uppercase"
+       :letter-spacing "0.1em"}]]
+
+    [:nav
+
+     [:a
+      {:color (data :color)
+       :opacity 0.6
+       :text-decoration "none"
+       :margin-left "2em"
+       :transition "opacity 0.1s ease-in-out"
+       :line-height "2em"}
+
+      [:&:hover
+       {:opacity 1}]
+
+      ["&[data-icon]:before"
+       {:content "attr(data-icon)"
+        :font-family "FontAwesome"
+        :margin-right "0.25em"}]
+
+      [:&.button
+       {:display "inline-block"
+        :border [["1px" "solid" (data :color)]]
+        :border-radius "0.25em"
+        :height "2em"
+        :padding "0 0.5em" }]]]]])
 
 (defn component [data]
   [:header
