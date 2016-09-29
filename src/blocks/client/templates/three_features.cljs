3aed597393c2e096f9b2c0df1088b24ec6f84bc6
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-09-28 21:45:36 -0400

    Add icons/images to three-features block

--- src/blocks/client/templates/three_features.cljs
+++ src/blocks/client/templates/three_features.cljs
@@ -8,33 +8,49 @@
    {:background (get-in data [:styles :background])
     :box-sizing "border-box"
     :color (get-in data [:heading :color])}
+
    [:.content
     {:padding "2em"}
+
     [:.columns
      {:display "flex"
       :justify-content "space-between"
-      :align-items "center"}
+      :align-items "flex-start"}
+
      [:.column
       {:text-align "center"
        :flex-basis (str (/ 100 (data :column)) "%")
        :min-width "14em"
-       :height "14em"
        :padding "0 1em"
        :box-sizing "border-box"}
+
+      [:img
+       {:padding "0.5em 0"
+        :height "3rem"}]
+
+      [:.icon::before
+ {:content (str "attr(data-icon)")
+  :font-family "FontAwesome"
+  :font-size "3em"
+  :height "4rem"
+  :line-height "4rem"}]
+
       [:h2
        {:font-weight "bolder"
         :font-size "1.25em"
         :padding "1em"}]
+
       [:p
        {:color "#fff"
         :font-size "1em"}]]]]
+
    (at-media {:max-width "700px"}
-             [:.content
-              [:.columns
-               {:display "flex"
-                :flex-direction "column"
-                :justify-content "center"
-                :align-items "center"}]])])
+     [:.content
+      [:.columns
+       {:display "flex"
+        :flex-direction "column"
+        :justify-content "center"
+        :align-items "center"}]])])
 
 (defn component [data]
   [:div.three-features
@@ -42,6 +58,10 @@
      [:div.columns
       (for [column (data :columns)]
         [:div.column
+         (when (column :icon)
+           [:div.icon {:data-icon (column :icon)}])
+         (when (column :image)
+           [:img {:src (column :image)}])
          [:h2 (column :title)]
          [:p (column :description)]])]]])
 
