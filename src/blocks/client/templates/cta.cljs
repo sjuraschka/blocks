589e1dabaa9658526d3ea56feb1082513f4feb9d
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-10-06 15:16:24 -0600

    Adjust spacing

--- src/blocks/client/templates/cta.cljs
+++ src/blocks/client/templates/cta.cljs
@@ -2,7 +2,7 @@
   (:require [blocks.client.template :refer [template]]
             [blocks.client.templates.partials.email-field :as email-field]))
 
-  (def pad "3em")
+  (def pad "6em")
 
   (defn styles [data]
     [:.cta
@@ -19,7 +19,7 @@
 
       [:p
        {:color (get-in data [:description :color])
-        :font-size "1.24rem"
+        :font-size "1.25rem"
         :margin-bottom "2em"}]
 
       (email-field/styles data)]])
