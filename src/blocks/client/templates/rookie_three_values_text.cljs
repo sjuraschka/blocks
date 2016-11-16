0d2222c40834d227df94231b51d25177802a8312
Sam Juraschka <samjuraschka@gmail.com>
2016-11-16 12:17:46 -0500

    Add title and fix alignment of columns

--- src/blocks/client/templates/rookie_three_values_text.cljs
+++ src/blocks/client/templates/rookie_three_values_text.cljs
@@ -4,55 +4,65 @@
 
 (defn styles [data]
   [:.three-values-text
-   {:background   "#fff"
-    :box-sizing "border-box"
+   {:background     "#fff"
+    :box-sizing     "border-box"
     :padding-bottom "1em"
-    :border-bottom "1px solid #eee"
-    ;:box-shadow  "2px 5px 0px rgba(168,168,168,1)"
-    }
+    :border-bottom  "1px solid #eee"}
 
    [:&::before
     {:content          "\"\""
      :display          "block"
      :background-image (data :background)
-     :height           "25em"
+     :height           "29em"
      :position         "relative"
      :width            "100vw"
      :z-index          1}]
 
    [:.content
-    {:margin-top "-22.2em"
-     :z-index    100
-     :padding    "1.5em 4em 0em 4em"
-     :box-sizing "border-box"
-     :width      "100vw"
-     :display    "flex"
-     :justify-content "center"}
+    {:margin-top     "-27em"
+     :z-index        100
+     :padding        "1.5em 4em 0em 4em"
+     :box-sizing     "border-box"
+     :width          "100vw"
+     :display        "flex"
+     :flex-direction "column"
+     :align-items    "center"}
+
+    [:.title {:font-size      "1.75em"
+              :box-sizing     "border-box"
+              :color          "#fff"
+              :padding-bottom "1.5em"}]
 
     [:.columns
      {:display         "flex"
+      :text-align      "center"
       :justify-content "space-between"
       :width           "100vw"}
 
      [:.column
-      {:min-width      "30vw"
-       :text-align "center"}]]
+      {:width      "30vw"
+       :box-sizing     "border-box"
+       :text-align     "center"
+       :display        "flex"
+       :flex-direction "column"
+       :align-items    "center"}
 
-    [:h1
-     {:padding-top "1em"
-      :color (data :title-color)
-      :text-transform "uppercase"}]
+      [:h1.section-title
+       {:padding-top    "1em"
+        :color          (data :title-color)
+        :text-transform "uppercase"}]
 
+      [:p
+       {:color     "#696969"
+        :box-sizing "border-box"
+        :padding   "1em 2em"
+        :width "28vw"
+        :font-size "0.9rem"}]
 
-    [:p
-     {:color     "#696969"
-      :padding   "1em 3em"
-      :font-size "0.9rem"}]
-
-    [:a
-     {:color     "blue"
-      :weight    "bolder"
-      :font-size "0.9rem"}]]
+      [:a
+       {:color     "blue"
+        :weight    "bolder"
+        :font-size "0.9rem"}]]]]
 
    (at-media {:max-width "900px"}
 
@@ -62,50 +72,40 @@
              [:.content
               {:margin-top 0}
 
-               [:.columns
-                {:flex-direction "column"}]
+              [:.title
+               {:color       (data :title-color)
+                :text-align  "center"
+                :font-size   "1.3em"
+                :font-weight "bold"}]
 
-                  [:column
-                   {:width "80vw"}]
+              [:.columns
+               {:flex-direction "column"
+                :width          "100vw"}
 
-                  [:h1
-                   {:padding-top "2em"}]
+               [:.column
+                {:width "100vw"
+                 :justify-content "center"}
 
-                  [:p
-                   {:padding       "1em 3em 2em 3em"
-                    :border-bottom "1px solid #ccc"
-                    :margin-bottom "2em"}]])])
+                [:p
+                 {:width "100%"
+                  :padding "1em 2em"
+                  :border-bottom "1px solid #eee"
+                  :margin-bottom "1em"}]]]])])
 
 (defn component [data]
   [:div.three-values-text
    [:div.content
+    [:h1.title (data :header)]
     (into
       [:div.columns]
       (for [column (data :columns)]
         [:div.column
          [:img {:src (column :url)}]
-         [:h1 (get-in column [:title :name])]
+         [:h1.section-title (get-in column [:title :name])]
          [:p (column :subtitle)]]))]])
-
-
 
 (defmethod template "rookie-three-values-text" [_]
   {:css       styles
    :component component})
 
 
-#_ (into
-  [:.columns
-   {:flex-direction "column"}]
-
-  (for [column (data :columns)]
-    [:column
-     {:width "80vw"}]
-
-    [:h1
-     {:padding-top "2em"}]
-
-    [:p
-     {:padding       "1em 3em 2em 3em"
-      :border-bottom (column :border)
-      :margin-bottom "2em"}]))
