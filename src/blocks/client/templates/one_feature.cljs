83834417e9e6f82aa5ae153f5583c7484f4d7b50
Sam Juraschka <samjuraschka@gmail.com>
2016-11-25 16:09:20 -0500

    Reduce hard-coded data for one-feature template

--- src/blocks/client/templates/one_feature.cljs
+++ src/blocks/client/templates/one_feature.cljs
@@ -3,30 +3,32 @@
 
 (defn styles [data]
   [:.one-feature
-   {:background "65A8B6"
+   {:background (get-in data [:background :color])
     :display "flex"
     :flex-direction "column"
     :justify-content "center"
     :align-items "center"
     :text-align "center"
-    :box-sizing "border-box"}
+    :box-sizing "border-box"
+    :padding-top (get-in data [:page-padding :top])}
 
    [:.content
     {:padding "3em"}
     [:h1
      {:color (get-in data [:heading :color])
-      :font-size "2.5em"
-      :font-weight "bolder"}]
-    [:h2
+      :font-size "1.75em"
+      :font-weight "bolder"
+      :text-transform (get-in data [:heading :transform])}]
+    [:p
      {:color (get-in data [:description :color])
-      :font-size "1.25em"
+      :font-size "1em"
       :padding "0.5em"}]]])
 
 (defn component [data]
   [:div.one-feature
    [:div.content
    [:h1 (get-in data [:heading :text])]
-   [:h2 (get-in data [:description :text])]]])
+   [:p (get-in data [:description :text])]]])
 
 (defmethod template "one-feature" [_]
   {:css styles
