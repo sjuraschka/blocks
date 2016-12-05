7a52c30c0b00f1c447e3bcc0cd797c3a8d16e42a
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-12-05 16:48:32 -0500

    Show a loading spinner on email field

--- src/blocks/client/styles.cljs
+++ src/blocks/client/styles.cljs
@@ -2,7 +2,7 @@
   (:require
     [clojure.string :as string]
     [garden.core :refer [css]]
-    [garden.stylesheet :refer [at-import at-font-face]]))
+    [garden.stylesheet :refer [at-import at-font-face at-keyframes]]))
 
 (defn google-fonts-import [data]
    (when-let [google-fonts (->> (get-in data [:styles :fonts])
@@ -33,6 +33,10 @@
 
 (defn page-styles [data]
   [(google-fonts-import data)
+
+   (at-keyframes :anim-spin
+     ["0%" {:transform "rotate(0deg)"}]
+     ["100%" {:transform "rotate(359deg)"}])
 
    fa-at-font-face
 
--- src/blocks/client/templates/partials/email_field.cljs
+++ src/blocks/client/templates/partials/email_field.cljs
@@ -1,7 +1,7 @@
 (ns blocks.client.templates.partials.email-field
   (:require
     [reagent.core :as r]
-    [blocks.client.templates.mixins :refer [button-mixin fontawesome-mixin]]
+    [blocks.client.templates.mixins :refer [button-mixin fontawesome-mixin spin-mixin]]
     [garden.stylesheet :refer [at-media]]
     [ajax.core :refer [ajax-request keyword-request-format keyword-response-format]])
   (:import
@@ -34,14 +34,17 @@
      :overflow "hidden"}
 
     [:input
-     {:padding "0 0.75em"
-      :border "none"
-      :box-sizing "border-box"
-      :background (get-in data [:form :background])
+     :button
+     {:box-sizing "border-box"
       :height "3rem"
       :vertical-align "top"
       :line-height "3rem"
-      :outline "none"}
+      :outline "none"}]
+
+    [:input
+     {:border "none"
+      :padding "0 0.75em"
+      :background (get-in data [:form :background])}
 
      ["&[type=email]"
       {:font-size "1.25em"
@@ -51,69 +54,83 @@
        {:color "#ddd"}]
 
       ["&::-webkit-input-placeholder"
-       {:color "#ddd"}]]
+       {:color "#ddd"}]]]
 
-     ["&[type=submit]"
-      (button-mixin (get-in data [:button :colors] {}))
-      [:&
-       {:border-radius 0
-        :font-size "1em"
-        :padding "0 1em"
-        :letter-spacing "0.05em"
-        :text-transform "uppercase"}]]]]
+    [:button
+     (button-mixin (get-in data [:button :colors] {}))
+     [:&
+      {:border-radius 0
+       :font-size "1em"
+       :-webkit-font-smoothing "antialiased"
+       :padding "0 1em"
+       :letter-spacing "0.05em"
+       :text-transform "uppercase"}]
+     [:&::after
+      (fontawesome-mixin \uf054) ; chevron-right
+      {:margin-left "0.5em"}]]]
 
+   [:&.loading
+    [:button::after
+     (fontawesome-mixin \uf110) ; spinner
+     (spin-mixin)]]
    (at-media {:max-width "450px"}
-             [:fieldset
-              {:display "flex"
-               :flex-direction "column"}
-              {}
-             [:input
-              {:width "100%"}]
+     [:fieldset
+      {:display "flex"
+       :flex-direction "column"}
+      {}
+      [:input
+       {:width "100%"}]
 
-              ["&[type=email]"
-               {:font-size "1em"
-                :min-width "9em"}]])])
+      ["&[type=email]"
+       {:font-size "1em"
+        :min-width "9em"}]])])
 
 (defn component [data]
   (let [email (r/atom "")
+        loading? (r/atom false)
         submitted? (r/atom false)
         error-message (r/atom nil)]
     (fn [data]
-      (if-not @submitted?
-        [:form {:on-submit (fn [e]
-                             (.preventDefault e)
-                             (if (not (re-matches #".+@.+\..+" @email))
-                               (reset! error-message "That doesn't look like an email. Please try again.")
-                               (ajax-request {:method (get-in data [:ajax :method])
-                                              :uri (get-in data [:ajax :action])
-                                              :params (merge
-                                                        (get-in data [:ajax :params])
-                                                        {:email (goog.string.urlEncode @email)})
-                                              ; allowed formats: transit, json, text, raw, url
-                                              :format (keyword-request-format (get-in data [:ajax :request-format] :raw) {})
-                                              ; allowed formats: transit, json, text, raw, detect
-                                              :response-format (keyword-response-format (get-in data [:ajax :response-format] :detect) {})
-                                              :handler (fn [[ok response]]
-                                                         (if ok
-                                                           (reset! submitted? true)
-                                                           (do
-                                                             (js/console.log response)
-                                                             (reset! error-message "Something went wrong. Please try again."))))})))}
+      (let [ajax-request! (fn []
+                           (ajax-request {:method (get-in data [:ajax :method])
+                                          :uri (get-in data [:ajax :action])
+                                          :params (merge
+                                                    (get-in data [:ajax :params])
+                                                    {:email (goog.string.urlEncode @email)})
+                                          ; allowed formats: transit, json, text, raw, url
+                                          :format (keyword-request-format (get-in data [:ajax :request-format] :raw) {})
+                                          ; allowed formats: transit, json, text, raw, detect
+                                          :response-format (keyword-response-format (get-in data [:ajax :response-format] :detect) {})
+                                          :handler (fn [[ok response]]
+                                                     (reset! loading? false)
+                                                     (if ok
+                                                       (reset! submitted? true)
+                                                       (reset! error-message "Something went wrong. Please try again.")))}))
+            submit-form! (fn []
+                           (reset! loading? true)
+                           (ajax-request!))]
 
-         [:fieldset
-          [:input {:type "email"
-                   :name "email"
-                   :value @email
-                   :placeholder (get-in data [:placeholder])
-                   :auto-focus (when (get-in data [:autofocus]) "autofocus")
-                   :on-change (fn [e]
-                                (reset! email (.. e -target -value)))}]
-          [:input {:type "submit"
-                   :value (get-in data [:button :text])}]]
-         (when (get-in data [:after :text])
-           [:p.after (get-in data [:after :text])])
-         (when @error-message
-           [:p.error @error-message])
-         (when (get-in data [:before :text])
-           [:p.before (get-in data [:before :text])])]
-        [:div (data :post-submit-message)]))))
+        (if @submitted?
+          [:div (data :post-submit-message)]
+          [:form {:class (str (when @loading? "loading") " "
+                              (when @error-message "error") " "
+                              "loading")
+                  :on-submit (fn [e]
+                               (.preventDefault e)
+                               (if (not (re-matches #".+@.+\..+" @email))
+                                 (reset! error-message "That doesn't look like an email. Please try again.")
+                                 (submit-form!)))}
+           (when (get-in data [:before :text])
+             [:p.before (get-in data [:before :text])])
+           [:fieldset
+            [:input {:type "email"
+                     :name "email"
+                     :value @email
+                     :placeholder (get-in data [:placeholder])
+                     :auto-focus (when (get-in data [:autofocus]) "autofocus")
+                     :on-change (fn [e]
+                                  (reset! email (.. e -target -value)))}]
+            [:button {} (get-in data [:button :text])]]
+           (when (get-in data [:after :text])
+             [:p.after (get-in data [:after :text])])
+           [:p.error (or @error-message " ")]])))))
