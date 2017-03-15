c62559f656ed47bbcd3b6f2ad5f9ddd27285dbe5
sjuraschka <samjuraschka@gmail.com>
2017-03-14 23:04:50 -0400

    Reorder templates alphabetically and add new template for text based documents

--- src/blocks/client/templates.cljs
+++ src/blocks/client/templates.cljs
@@ -1,27 +1,31 @@
 (ns blocks.client.templates
   (:require
-    blocks.client.templates.raw
     blocks.client.templates.analytics
+    blocks.client.templates.braid-conversations
+    blocks.client.templates.cta
+    blocks.client.templates.downloads
+    blocks.client.templates.features-grid
+    blocks.client.templates.feature-highlight
+    blocks.client.templates.footer
     blocks.client.templates.header
     blocks.client.templates.hero
     blocks.client.templates.hero-columns
     blocks.client.templates.hero-middle
     blocks.client.templates.hero-testimonial
-    blocks.client.templates.letter
-    blocks.client.templates.video
-    blocks.client.templates.downloads
-    blocks.client.templates.braid-conversations
-    blocks.client.templates.features-grid
-    blocks.client.templates.pricing
-    blocks.client.templates.footer
-    blocks.client.templates.roadmap
-    blocks.client.templates.cta
-    blocks.client.templates.feature-highlight
-    blocks.client.templates.one-feature
-    blocks.client.templates.three-features
-    blocks.client.templates.rookie-three-values-text
-    blocks.client.templates.pre-footer-menu
-    blocks.client.templates.multiple-logo-footer
-    blocks.client.templates.quote
     blocks.client.templates.how-it-works
-    blocks.client.templates.image-testimonial))
+    blocks.client.templates.image-testimonial
+    blocks.client.templates.letter
+    blocks.client.templates.multiple-logo-footer
+    blocks.client.templates.one-feature
+    blocks.client.templates.pre-footer-menu
+    blocks.client.templates.pricing
+    blocks.client.templates.quote
+    blocks.client.templates.raw
+    blocks.client.templates.roadmap
+    blocks.client.templates.rookie-three-values-text
+    blocks.client.templates.text
+    blocks.client.templates.three-features
+    blocks.client.templates.video))
+
+
+
