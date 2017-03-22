981cf9a192e8202e4b64850fd0514a97323269ba
sjuraschka <samjuraschka@gmail.com>
2017-03-22 16:27:11 -0400

    Add styles and data to center block template for about page

--- src/blocks/client/templates/center_text.cljs
+++ src/blocks/client/templates/center_text.cljs
@@ -3,25 +3,41 @@
 
 (defn styles [data]
   [:.center-text
-   {:background (get-in data [:background :color])
-    :display "flex"
-    :flex-direction "column"
-    :justify-content "center"
-    :align-items "center"
+   {:padding "3em"
+    :background (get-in data [:styles :background :color])
     :text-align "center"
+    :display "flex"
+    :justify-content "center"
     :box-sizing "border-box"}
 
-   [:h1]
-   [:h2]
-   [:h3]
-   [:p
-    {:color "#5f5f5f"}]])
+   [:.text
+    {:text-align "center"
+     :display "flex"
+     :flex-direction "column"
+     :justify-content "center"
+     :align-items "center"
+     :max-width "21em"}
+
+    [:.headline
+     {:color (get-in data [:styles :heading :color])
+      :font-size (get-in data [:styles :heading :size])
+      :margin "1em"}]
+
+    [:.label
+     {:color (get-in data [:styles :label :color])
+      :text-transform (get-in data [:styles :label :transform])
+      :font-size (get-in data [:styles :label :size])}]
+
+    [:p
+     {:color (get-in data [:styles :subtitle :color])
+      :font-size (get-in data [:styles :subtitle :size])}]]])
 
 (defn component [data]
   [:div.center-text
-   [:h3 (data :city)]
-   [:h2 (data :headline)]
-   [:p (data :description)]])
+   [:div.text
+    [:h3.label (data :city)]
+    [:h2.headline (data :headline)]
+    [:p.description (data :description)]]])
 
 (defmethod template "center-text" [_]
   {:css styles
