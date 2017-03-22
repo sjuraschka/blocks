4469913a82df2267237972ed676a44d314c211bd
sjuraschka <samjuraschka@gmail.com>
2017-03-22 10:34:28 -0400

    Update template to use data

--- src/blocks/client/templates/secondary_nav.cljs
+++ src/blocks/client/templates/secondary_nav.cljs
@@ -1,34 +1,40 @@
 (ns blocks.client.templates.secondary-nav
-  (:require [blocks.client.template :refer [template]]))
+  (:require [blocks.client.template :refer [template]]
+            [garden.stylesheet :refer [at-media]]))
 
 (defn styles [data]
   [:.secondary-nav
-   {:background (get-in data [:background :color])
+   {:background (get-in data [:styles :background :color])
     :text-align "center"
     :box-sizing "border-box"
-    :border-bottom "1px solid #eee"
-    :height "4em"}
+    :border-bottom (get-in data [:styles :border])
+    :height (get-in data [:container :height])}
 
    [:div.nav-content
-    {:padding "0 5em"
+    {:padding "0 4em"
      :display "flex"
      :height "100%"
-     :font-weight 900
+     :font-weight (get-in data [:styles :font-weight])
      :justify-content "center"
      :align-items "center"
-     :color (data :color)}
+     :color (get-in data [:styles :color])}
     [:.menu-items
      {:display "flex"
       :justify-content "center"
       :align-items "Center"
-      :font-size "0.85em"}
+      :font-size (get-in data [:styles :font-size])}
 
      [:.menu-item
       {:margin "0 2em"
-       :padding "0.5em 1em"}
+       :padding "0.5em 1em"
+       :border (get-in data [:styles :border])
+       :border-radius "30px"
+       :cursor "pointer"
+       :color (get-in data [:styles :color])
+       :text-decoration "none"}
       [:&:hover
-       {:border "1px solid #ccc"
-        :border-radius "30px"}]
+       {:background "#eee"
+        :border (get-in data [:styles :bold-border])}]
       [:&.final:hover
        {:background "#3D50CF"
         :color "#fff"}]]
@@ -36,7 +42,10 @@
       {:width "4em"
        :height "1px"
        :border "none"
-       :border-bottom "1px solid #ccc"}]]]])
+       :border-bottom "1px solid #ccc"}]]]
+   (at-media {:max-width "800px"}
+     [:&
+      {:display "none"}])])
 
 (defn component [data]
   [:div.secondary-nav
@@ -44,10 +53,12 @@
       (into
         (for [item (data :menu)]
          [:div.menu-items
-          [:div.menu-item.white (item :title)]
+          [:a.menu-item.white {:href (item :url)}
+           (item :title)]
           [:hr]]))
       [:div.menu-items
-       [:div.menu-item.final "Get Paid"]]]])
+       [:a.menu-item.final {:href (get-in data [:title-final :url])}
+        (get-in data [:title-final :text])]]]])
 
 (defmethod template "secondary-nav" [_]
   {:css styles
