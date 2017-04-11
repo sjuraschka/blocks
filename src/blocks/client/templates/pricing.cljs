6038050ee05a88758bd340f938218c23253d193b
sjuraschka <samjuraschka@gmail.com>
2017-04-11 15:28:01 -0400

    Update pricing template for new Braid design

--- src/blocks/client/templates/pricing.cljs
+++ src/blocks/client/templates/pricing.cljs
@@ -29,9 +29,9 @@
 
     [:.option
      {:background     "#fff"
-      :box-shadow     [[0 "1px" "2px" 0 "#ccc"]]
       :border-radius  "3px"
-      :border         "1px solid #eee"
+      :box-shadow     "0 8px 15px 0px rgba(0,0,0,0.05)"
+      :border         "1px solid #fafafa"
       :box-sizing     "border-box"
       :min-width      "18em"
       :height         "25em"
@@ -45,64 +45,62 @@
       {:margin-right 0}]
 
      [:&.highlight
-      {:border "2px solid #f17130"
+      {;:border "3px solid #3D75FB"
        :height "30em"}
 
       [:.featured
-       {:background "#f17130"
+       {:background "#3D75FB"
         :width "100%"
         :padding "0.25em"
         :box-sizing "border-box"
         :color "#fff"}]]
-
 
      [:.about
       {:height "15%"
        :padding "1em 1em 5em 1em"}
 
       [:h1
-      {:color       (get-in data [:title-color])
-       :font-weight "bolder"
-       :font-size   "1.25em"}]
+       {:color       (get-in data [:title-color])
+        :font-weight "bolder"
+        :font-size   "1.25em"}]
       [:h2
        {}]
 
-     [:.price
-      {:margin-top "1em"}
+      [:.price
+       {:margin-top "1em"}
 
-      [:.amount
-       {:color     "#384d51"
-        :font-size "2.5em;"}
+       [:.amount
+        {:color     "#384d51"
+         :font-size "2.5em;"}
 
-       [:&::before
-        {:content "\"$\""
-         :font-size "0.5em"
-         :line-height "1.5em"
-         :vertical-align "top"}]]
+        [:&::before
+         {:content "\"$\""
+          :font-size "0.5em"
+          :line-height "1.5em"
+          :vertical-align "top"}]]
 
-      [:input {:border-radius "0.25em"
-               :background "#f4f8fb"
-               :margin "0 0.25em 0.25em 0.25em"
-               :border "1px solid #ccc"
-               :font-size "2.25rem"
-               :color "#384d51"
-               :width "7rem"
-               :box-sizing "border-box"
-               :text-align "center"}]
+       [:input {:border-radius "3px"
+                :background "#f4f7ff"
+                :margin "0 0.25em 0.25em 0.25em"
+                :border "1px solid #3D75FB"
+                :font-size "2.25rem"
+                :color "#384d51"
+                :width "7rem"
+                :box-sizing "border-box"
+                :text-align "center"}]
+       
+       [:a.button
+        (button-mixin (get-in data [:button-colors]))
+        [:&
+         {:border-radius "0.25em"
+          :font-size "1em"
+          :padding "0.5em 1em"
+          :margin "10px"
+          :text-transform "uppercase"}]]
 
-
-      [:a.button
-       (button-mixin (get-in data [:button-colors]))
-       [:&
-        {:border-radius "0.25em"
-         :font-size "1em"
-         :padding "0.5em 1em"
-         :margin "10px"
-         :text-transform "uppercase"}]]
-
-      [:.text
-       {:color "#5f6c73"
-        :font-size "0.8em"}]]]
+       [:.text
+        {:color "#5f6c73"
+         :font-size "0.8em"}]]]
 
      [:.features
       {:width       "100%"
@@ -110,8 +108,8 @@
        :font-size "0.9em"}
       [:h1
        {:color "#5f6c73"
-        :font-weight "bolder"
-        }]
+        :font-weight "bolder"}]
+
 
       [:.feature
        {:padding-top "0.25em"
@@ -127,7 +125,6 @@
                {:display "flex"
                 :flex-direction "column"
                 :flex-wrap "wrap"}]])])
-
 
 (defn component [data]
   [:section.pricing
@@ -165,9 +162,9 @@
          (into
            [:div]
            (for [feature (option :features)]
-             [:div.feature feature]))]
+             [:div.feature feature]))]]))])
         ;[:a.button "Start Trial"]
-        ]))])
+
 
 (defmethod template "pricing" [_]
   {:css       styles
