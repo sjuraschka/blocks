9039fc3f7e2cb1fb88e78565bfdb541673d59fb0
sjuraschka <samjuraschka@gmail.com>
2017-04-11 15:26:50 -0400

    Update Braid Call to Action styles to be a focus of the website

--- src/blocks/client/templates/cta.cljs
+++ src/blocks/client/templates/cta.cljs
@@ -2,36 +2,36 @@
   (:require [blocks.client.template :refer [template]]
             [blocks.client.templates.partials.email-field :as email-field]))
 
-  (def pad "6em")
+(def pad "6em")
 
-  (defn styles [data]
-    [:.cta
-     {:padding pad
-      :background (get-in data [:background :color])
-      :text-align "center"}
+(defn styles [data]
+  [:.cta
+   {:padding pad
+    :background (get-in data [:background :color])
+    :text-align "center"}
 
-     [:.content
+   [:.content
 
-      [:h1
-       {:color (get-in data [:heading :color])
-        :font-weight "bolder"
-        :font-size "2.2em"}]
+    [:h1
+     {:color (get-in data [:heading :color])
+      :font-weight "bolder"
+      :font-size "2.2em"}]
 
-      [:p
-       {:color (get-in data [:description :color])
-        :font-size "1.25rem"
-        :margin-bottom "2em"}]
+    [:p
+     {:color (get-in data [:description :color])
+      :font-size "1.25rem"
+      :margin-bottom "2em"}]
 
-      (email-field/styles (data :form))]])
+    (email-field/styles (data :form))]])
 
-  (defn component [data]
-    [:section.cta
-      [:div.content
-        [:div.text
-          [:h1 (get-in data [:heading :text])]
-          [:p  (get-in data [:description :text])]]
-        [email-field/component (data :form)]]])
+(defn component [data]
+  [:section.cta
+    [:div.content
+      [:div.text
+        [:h1 (get-in data [:heading :text])]
+        [:p  (get-in data [:description :text])]]
+      [email-field/component (data :form)]]])
 
-  (defmethod template "cta" [_]
-    {:css styles
-     :component component})
+(defmethod template "cta" [_]
+  {:css styles
+   :component component})
