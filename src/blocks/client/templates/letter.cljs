d9968595bac582852bab0430274dee712becc61e
sjuraschka <samjuraschka@gmail.com>
2017-04-11 15:00:45 -0400

    Update styles and layout for letter template to include images, headlines and a signature

--- src/blocks/client/templates/letter.cljs
+++ src/blocks/client/templates/letter.cljs
@@ -10,19 +10,36 @@
      :flex-direction "column"
      :justify-content "center"
      :align-items "center"
-     :padding "3em"}
+     :padding "1em"}
 
-    [:h1
-     {:font-size "1.25em"
+    [:h1.title
+     {:font-size "1.85em"
       :padding "1em"
       :color (get-in data [:heading :title-color])
       :font-weight "bolder"}]
-    [:.text
-     {:padding "2em"
-      :color (get-in data [:heading :title-color])
-      :width :80%
-      :column-count 2
-      :column-gap "1em"}]]
+
+    [:.lower
+     {:display "flex"
+      :width "90%"}
+
+     [:div.left
+      {:position "relative"
+       :width "60%"}
+
+      [:.text-content
+       {:padding-bottom "1em"
+        :line-height "1.25em"
+        :color (get-in data [:paragraph :color])}
+       [:span
+        {:font-weight 600}]]
+      [:.signature
+       {:height "4em"
+        :padding "1em 2em"}]]
+
+     [:div.spacer
+      {:width "5%"}]
+     [:.right
+      {:width "35%"}]]]
 
    (at-media {:max-width "575px"}
              [:.content
@@ -35,8 +52,16 @@
 (defn component [data]
   [:div.letter
    [:div.content
-    [:h1 (get-in data [:heading :text])]
-    [:div.text (get-in data [:paragraph :text])]]])
+    [:h1.title (get-in data [:heading :text])]
+
+    [:div.lower
+     [:div.left
+      [:div.text-content {:dangerouslySetInnerHTML
+                           {:__html (get-in data [:paragraph :text])}}]
+      [:img.signature {:src "/braid/images/braid-chat-signature.png"}]]
+     [:div.spacer]
+     [:div.right
+      [:img {:src "/braid/images/braid-chat-thread.png"}]]]]])
 
 (defmethod template "letter" [_]
   {:css       styles
